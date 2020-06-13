92032f1619b792b13babbaa64cdc84d1bf54384e
Vigneshwaran <vigneshwaran0310@gmail.com>
2020-06-13 14:19:53 -0400

    Checking MongoDB shrads

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-v1oy1.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-shard-00-00-v1oy1.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
   )
   .then(result => {
     app.listen(process.env.PORT || 8080);
